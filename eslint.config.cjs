4fff8cc1ce0786f916b3adcbd4f5f73f8b4e6c01
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-15 04:35:02 +0000

    Setup eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,42 @@
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    files: ['./src/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ['*.config.js', '*.config.cjs', 'webpack.*.cjs'],
+    languageOptions: {
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  prettier,
+];
