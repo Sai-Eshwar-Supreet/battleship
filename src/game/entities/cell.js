8cb3d432bedf5e81d5624539b23fb637a5d71de7
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-16 15:33:47 +0000

    Add cell module

---
+++ src/game/entities/cell.js
@@ -0,0 +1,32 @@
+import { assertEnum } from "../../core/validation/enum-validation";
+
+class Cell{
+    #occupiedShipId;
+    #state;
+
+    constructor(){
+        this.#occupiedShipId = null;
+        this.#state = Cell.cellState.clean;
+    }
+
+    get occupiedShipId() {
+        return this.#occupiedShipId;
+    }
+
+    get state(){
+        return this.#state;
+    }
+
+    set state(value){
+        assertEnum(Cell.cellState, value, 'Cell State');
+        this.#state = value;
+    }
+
+    static cellState = Object.freeze({
+        hit: 'hit',
+        miss: 'miss',
+        clean: 'clean'
+    });
+}
+
+export { Cell }
