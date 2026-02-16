f326d2c20fd2574a8c36ed1710ac76794f0d5f20
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-16 15:33:21 +0000

    Add enum-validation module

---
+++ src/core/validation/enum-validation.js
@@ -0,0 +1,7 @@
+function assertEnum(enumObj, value, type = "value"){
+  if (!Object.values(enumObj).includes(value)) {
+    throw new Error(`Invalid ${type}: ${value}`);
+  }
+}
+
+export { assertEnum }
