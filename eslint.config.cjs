8e9710f519f52b9fd43743c6488ffa0138604787
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-02-21 02:22:15 +0000

    Update eslint config
    - ignore unused arguments starting with '_'
    - ignore unused variables starting with '_'
    - ignore unused caught errors starting with '_'

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -19,6 +19,13 @@ module.exports = [
     },
     rules: {
       'no-console': 'warn',
+      'no-unused-vars': [
+        'error', {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        }
+      ]
     },
   },
   {
