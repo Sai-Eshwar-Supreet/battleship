313754e25fa79f7ef97ce4775cb69e6b5c074d4b
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-07 23:38:45 +0000

    Add game-controller module

---
+++ src/game/controllers/game-controller.js
@@ -0,0 +1,53 @@
+import {StateMachine} from '../../core/state/state-machine.js';
+import { FLEET_CONFIG } from '../config/game-config.js';
+import { Ship } from '../entities/ship.js';
+import { createMenuState } from '../states/menu-state-factory.js';
+import { createPlacementState } from '../states/placement-state-factory.js';
+
+class GameController{
+    #stateMachine;
+    #players;
+
+    constructor(){
+        this.#stateMachine = new StateMachine();
+        this.#players = {};
+
+        //register states
+        this.#stateMachine.addState(createMenuState, this);
+        this.#stateMachine.addState(createPlacementState, this);
+    }
+    
+    start(){
+        this.#stateMachine.start('MENU');
+    }
+
+    update(dt){
+        this.#stateMachine.update(dt);
+    }
+
+    requestState(type){
+        this.#stateMachine.requestState(type);
+    }
+
+    getState(){
+        return this.#stateMachine.currentStateType;
+    }
+
+    get players(){
+        return this.#players;
+    }
+
+    buildFleet(){
+        const ships = [];
+        for(let shipData of FLEET_CONFIG){
+            const ship = new Ship(shipData);
+
+            ships.push(ship);
+        }
+
+        return ships;
+    }
+
+}
+
+export {GameController};
