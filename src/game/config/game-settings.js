38d9911f3ac35ee7c23c312fbe86cd8bcb1d4183
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-07 23:37:35 +0000

    Add game settings module

---
+++ src/game/config/game-settings.js
@@ -0,0 +1,29 @@
+import { DIFFICULTY } from "./difficulty-config";
+
+let playerName = 'Player';
+let difficulty = DIFFICULTY.NORMAL;
+
+function getSettings(){
+    return { playerName , difficulty};
+}
+
+function setSettings(name, difficultyId){
+    if(typeof name !== 'string'){
+        throw new TypeError("Expects a string playerName");
+    }
+
+    if(typeof difficultyId !== 'string'){
+        throw new TypeError("Expects a string difficultyId");
+    }
+    
+    const newDifficulty = Object.values(DIFFICULTY).find(d => d.id === difficultyId);
+
+    if(!newDifficulty){
+        throw new Error('Difficulty id is invalid');
+    }
+
+    playerName = name;
+    difficulty = newDifficulty;
+}
+
+export {getSettings, setSettings};
